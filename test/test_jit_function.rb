d836064ae8aef746bbd78299afa37ce1b068b98b
cout <cout@97706e15-5354-4c53-a9c4-6d7fdcecbee1>
2008-02-25 21:38:35 +0000

    Added some more tests.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ruby-libjit/trunk@81 97706e15-5354-4c53-a9c4-6d7fdcecbee1

--- test/test_jit_function.rb
+++ test/test_jit_function.rb
@@ -158,5 +158,130 @@ class TestJitFunction < Test::Unit::TestCase
 
     assert_equal(4, function.apply(0, 0))
   end
+
+  def test_while_true_enters_loop
+    function = nil
+    JIT::Context.build do |context|
+      signature = JIT::Type.create_signature(
+          JIT::ABI::CDECL,
+          JIT::Type::INT,
+          [ ])
+      function = JIT::Function.compile(context, signature) do |f|
+        true_value = f.const(JIT::Type::INT, 1)
+        false_value = f.const(JIT::Type::INT, 0)
+        f.while(proc { true_value }) {
+          f.insn_return true_value
+        }.end
+        f.insn_return false_value
+      end
+    end
+
+    assert_equal(1, function.apply)
+  end
+
+  def test_while_true_reenters_loop
+    function = nil
+    JIT::Context.build do |context|
+      signature = JIT::Type.create_signature(
+          JIT::ABI::CDECL,
+          JIT::Type::INT,
+          [ ])
+      function = JIT::Function.compile(context, signature) do |f|
+        value = f.value(JIT::Type::INT)
+        value.store(f.const(JIT::Type::INT, 0))
+        f.while(proc { value < f.const(JIT::Type::INT, 2) }) {
+          value.store(value + f.const(JIT::Type::INT, 1))
+        }.end
+        f.insn_return value
+      end
+    end
+
+    assert_equal(2, function.apply)
+  end
+
+  def test_while_false_does_not_enter_loop
+    function = nil
+    JIT::Context.build do |context|
+      signature = JIT::Type.create_signature(
+          JIT::ABI::CDECL,
+          JIT::Type::INT,
+          [ ])
+      function = JIT::Function.compile(context, signature) do |f|
+        true_value = f.const(JIT::Type::INT, 1)
+        false_value = f.const(JIT::Type::INT, 0)
+        f.while(proc { false_value }) {
+          f.insn_return true_value
+        }.end
+        f.insn_return false_value
+      end
+    end
+
+    assert_equal(0, function.apply)
+  end
+
+  def test_until_false_enters_loop
+    function = nil
+    JIT::Context.build do |context|
+      signature = JIT::Type.create_signature(
+          JIT::ABI::CDECL,
+          JIT::Type::INT,
+          [ ])
+      function = JIT::Function.compile(context, signature) do |f|
+        true_value = f.const(JIT::Type::INT, 1)
+        false_value = f.const(JIT::Type::INT, 0)
+        f.until(proc { false_value }) {
+          f.insn_return true_value
+        }.end
+        f.insn_return false_value
+      end
+    end
+
+    assert_equal(1, function.apply)
+  end
+
+  def test_until_false_reenters_loop
+    function = nil
+    JIT::Context.build do |context|
+      signature = JIT::Type.create_signature(
+          JIT::ABI::CDECL,
+          JIT::Type::INT,
+          [ ])
+      function = JIT::Function.compile(context, signature) do |f|
+        value = f.value(JIT::Type::INT)
+        value.store(f.const(JIT::Type::INT, 0))
+        f.until(proc { value == f.const(JIT::Type::INT, 2) }) {
+          value.store(value + f.const(JIT::Type::INT, 1))
+        }.end
+        f.insn_return value
+      end
+    end
+
+    assert_equal(2, function.apply)
+  end
+
+  def test_until_true_does_not_enter_loop
+    function = nil
+    JIT::Context.build do |context|
+      signature = JIT::Type.create_signature(
+          JIT::ABI::CDECL,
+          JIT::Type::INT,
+          [ ])
+      function = JIT::Function.compile(context, signature) do |f|
+        true_value = f.const(JIT::Type::INT, 1)
+        false_value = f.const(JIT::Type::INT, 0)
+        f.until(proc { true_value }) {
+          f.insn_return true_value
+        }.end
+        f.insn_return false_value
+      end
+    end
+  end
+
+  # TODO: while/break
+  # TODO: while/redo
+  # TODO: until/break
+  # TODO: until/redo
+  # TODO: unless
+  # TODO: elsunless
 end
 
