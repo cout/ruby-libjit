75b7c4254b4f1a57c42ff8fd18425e08f7efadcd
Paul Brannan <curlypaul924@gmail.com>
2008-11-01 09:35:45 -0400

    Added documentation.

--- lib/jit/array.rb
+++ lib/jit/array.rb
@@ -2,10 +2,34 @@ require 'jit'
 require 'jit/value'
 
 module JIT
+
+  # An abstraction for a fixed-length array type.
+  #
+  # Example usage:
+  #
+  #   array_type = JIT::Array.new(JIT::Type::INT, 4)
+  #
+  #   JIT::Context.build do |context|
+  #     signature = JIT::Type.create_signature(
+  #         JIT::ABI::CDECL, JIT::Type::INT, [ ])
+  #
+  #     function = JIT::Function.compile(context, signature) do |f|
+  #       array_instance = array_type.create(f)
+  #       array_instance[0] = f.const(JIT::Type::INT, 42)
+  #       f.insn_return(array_instance[0])
+  #     end
+  #
+  #   end
+  #
   class Array < JIT::Type
     attr_reader :type
     attr_reader :length
 
+    # Create a new JIT array type.
+    #
+    # +type+::   The type of the elements in the array.
+    # +length+:: The number of elements in the array.
+    #
     def self.new(type, length)
       array = self.create_struct([ type ] * length)
       array.instance_eval do
@@ -15,24 +39,42 @@ module JIT
       return array
     end
 
-    def wrap(function, ptr)
-      return Instance.wrap(self, function, ptr)
+    # Wrap an existing array.
+    #
+    # +ptr+:: A pointer to the first element in the array.
+    #
+    def wrap(ptr)
+      return Instance.wrap(self, ptr)
     end
 
+    # Create a new array.
+    #
+    # +function+:: The JIT::Function this array will be used in.
+    #
     def create(function)
       instance = function.value(self)
       ptr = function.insn_address_of(instance)
-      return wrap(function, ptr)
+      return wrap(ptr)
     end
 
+    # Return the offset (in bytes) of the element at the given +index+.
+    #
+    # +index+:: The index of the desired element.
+    #
     def offset_of(index)
       return self.get_offset(index)
     end
 
+    # Return the type of the element at the given +index+.
+    #
+    # +index+:: The index of the desired element.
+    #
     def type_of(index)
       return @type
     end
 
+    # An abstraction for an instance of a fixed-length array.
+    #
     class Instance < JIT::Value
       attr_reader :array_type
       attr_reader :type
@@ -41,19 +83,29 @@ module JIT
       # TODO: This breaks code below?
       # attr_reader :function
 
-      def self.wrap(array_type, function, ptr)
+      # Wrap an existing array.
+      #
+      # +array_type+:: The JIT::Array type to wrap.
+      # +ptr+::        A pointer to the first element in the array.
+      #
+      def self.wrap(array_type, ptr)
         pointer_type = JIT::Type.create_pointer(array_type)
-        value = self.new_value(function, pointer_type)
+        value = self.new_value(ptr.function, pointer_type)
         value.store(ptr)
         value.instance_eval do
           @array_type = array_type
           @type = array_type.type
-          @function = function
+          @function = ptr.function
           @ptr = ptr
         end
         return value
       end
 
+      # Generate JIT code to retrieve the element at the given +index+.
+      #
+      # +index+:: The index of the desired element.  The value of the
+      #           index must be known at compile-time.
+      #
       def [](index)
         @function.insn_load_relative(
             @ptr,
@@ -61,6 +113,12 @@ module JIT
             @array_type.type_of(index))
       end
 
+      # Generate JIT code to assign to the element at the given +index+.
+      #
+      # +index+:: The index of the desired element.  The value of the
+      #           index must be known at compile-time.
+      # +value+:: The JIT::Value to assign to the element.
+      #
       def []=(index, value)
         @function.insn_store_relative(
             @ptr,
