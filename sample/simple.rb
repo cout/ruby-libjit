fde3540e829fa4ec08a5c761393e1c32fe11bf99
Paul Brannan <cout@sapphire.(none)>
2009-12-02 19:23:57 -0500

    Simplified some more.

--- sample/simple.rb
+++ sample/simple.rb
@@ -1,13 +1,9 @@
 require 'jit'
 
-multiply = nil
-JIT::Context.build do |context|
-  multiply = context.compile_function([:INT, :INT] => :INT) do |f|
-    lhs = f.get_param(0)
-    rhs = f.get_param(1)
-    result = f.insn_mul(lhs, rhs)
-    f.insn_return(result)
-  end
+multiply = JIT::Function.build([:INT, :INT] => :INT) do |f|
+  lhs = f.get_param(0)
+  rhs = f.get_param(1)
+  f.return(lhs * rhs)
 end
 
 p multiply.apply(6, 7) #=> 42
